86bc6414a5b79650c455e0e2d78cf00c5f229d7d
Purpurova-k <katrin.purpurova@gmail.com>
2022-06-06 18:26:06 +0300

    Dashboard Cypress connected

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
+    projectId: "kx2fba",
     baseUrl: 'http://qamid.tmweb.ru/client/index.php',
     retries: 3,
   },
